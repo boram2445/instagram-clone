230b70698357a9639219c0502f81f0a3e5cb64a0
Boram Kim <boram2445@gmail.com>
2023-08-09 11:51:31 +0900

    Fix: 버튼 클릭시 팔로워, 팔로잉 수 반영

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -2,11 +2,18 @@
 
 import useMe from '@/hooks/useMe';
 import { ProfileUser } from '@/model/user';
+import { useRouter } from 'next/navigation';
+import { useState, useTransition } from 'react';
+import { PulseLoader } from 'react-spinners';
 
 type Prop = { user: ProfileUser };
 
 export default function FollowButton({ user }: Prop) {
   const { user: loggedInUser, toggleFollow } = useMe();
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isUpdating = isPending || isFetching;
 
   const showButton = loggedInUser && loggedInUser.username !== user.username;
   const following =
@@ -14,19 +21,35 @@ export default function FollowButton({ user }: Prop) {
     loggedInUser.following.find((item) => item.username === user.username);
 
   const text = following ? 'Unfollow' : 'Follow';
-  const handleFollow = () => toggleFollow(user.id, !Boolean(following));
+  const handleFollow = async () => {
+    setIsFetching(true);
+    await toggleFollow(user.id, !Boolean(following));
+    setIsFetching(false);
+    startTransition(() => {
+      //팔로우 정보가 처리되면, 페이지를 리프레시해라
+      router.refresh();
+    });
+  };
 
   return (
     <>
       {showButton && (
-        <button
-          onClick={handleFollow}
-          className={`py-0.5 px-3 ${
-            following ? 'bg-orange-500' : 'bg-green-500'
-          } rounded-full text-white hover:brightness-95`}
-        >
-          {text}
-        </button>
+        <div className='relative'>
+          {isUpdating && (
+            <div className='absolute inset-0 flex justify-center items-center'>
+              <PulseLoader size={6} />
+            </div>
+          )}
+          <button
+            disabled={isUpdating}
+            onClick={handleFollow}
+            className={`py-0.5 px-3 ${
+              following ? 'bg-orange-500' : 'bg-green-500'
+            } rounded-full text-white hover:brightness-95 disabled:opacity-70`}
+          >
+            {text}
+          </button>
+        </div>
       )}
     </>
   );
